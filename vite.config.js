c4102310df206570213edc5462227454fad1b191
yama-lei <1908777046@qq.com>
2025-03-28 21:29:53 +0800

    chore: initial commit with project setup

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
+// 根据当前环境确定基础路径
+const base = process.env.NODE_ENV === 'production' ? '/vqa-website/' : '/'
+
 export default defineConfig({
   plugins: [vue()],
-  base: '/vqa-website/',
+  base,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
