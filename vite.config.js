80b14280e940e234da7da9812258a27c22221b6a
yama-lei <1908777046@qq.com>
2025-03-28 21:34:19 +0800

    fix: update build configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,11 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 }) 
