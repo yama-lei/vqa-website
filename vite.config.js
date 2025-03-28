b599fa307366190e7e65e3cd591bc1f6cfef6ddb
yama-lei <1908777046@qq.com>
2025-03-28 21:37:43 +0800

    fix: update base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 // 根据当前环境确定基础路径
-const base = process.env.NODE_ENV === 'production' ? '/vqa-website/' : '/'
+const base = process.env.NODE_ENV === 'production' ? '/' : '/'
 
 export default defineConfig({
   plugins: [vue()],
